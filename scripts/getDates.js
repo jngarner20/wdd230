d83f9332674fc6e4429c989e0acb7c8e584e2a87
jngarner20 <jngarner20@gmail.com>
2024-05-14 11:13:38 -0600

    dlkd

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,25 +1,41 @@
-const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedElement = document.getElementById("lastModified");
 
 // Get the current date
 const currentDate = new Date();
 
 // Get the last modified date in the desired format
-const lastModifiedDate = currentDate.toLocaleString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric'
+const lastModifiedDate = currentDate.toLocaleString("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
 });
 
 // Set the text content of the lastModified element
 lastModifiedElement.textContent = `Last Modified: ${lastModifiedDate}`;
 
-
-//js for 
+//js for
 const currentYear = new Date().getFullYear();
 
-const copyrightElement = document.querySelector('footer p:first-child');
+const copyrightElement = document.querySelector("footer p:first-child");
 
 copyrightElement.textContent = `©${currentYear} Jessica Garner Colorado`;
+
+//Dark Mode
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("🕶️")) {
+    body.style.background = "#000";
+    body.style.color = "#fff";
+    modeButton.textContent = "🔆";
+  } else {
+    body.style.background = "#fff";
+    body.style.color = "#000";
+    Headers.style.background - "#ff97ab";
+    modeButton.textContent = "🕶️";
+  }
+});
